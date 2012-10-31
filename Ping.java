5324539890396c1a0fe103034f1edbb9cab1e151
Alpha4 <aure.b21@gmail.com>
2012-10-31 12:36:50 +0100

    Fusion de ping et pingpong
    Fusion de ping et pingpong --> pingpong insérer dans le main de ping
    Commentaires standardisés en début de classe
    Ajout de la fonction setPts pour la classe Joueur

--- Ping.java
+++ Ping.java
@@ -6,9 +6,12 @@ import java.util.Scanner;
 *@date 12/10/2012
 */
 
-// Déclaration de la classe joueur et ses fonctions associées
 public class Ping{
 	Scanner sc= new Scanner(System.in);
+	
+	/* Classe : Joueur
+	Méthodes : Constructeur, affichage, setPts
+	*/
 	public class Joueur
 	{
 		private String nom;
@@ -34,10 +37,18 @@ public class Ping{
 			System.out.println("Nombre de points après la compétition : "+pts);
 			System.out.println("---------------");
 		}
+		
+		public void setPts(int pts){
+			this.pts=pts;
+		}
 	}
+	
+	/* Classe : Classement
+	Méthodes : Constructeur et tri
+	*/
 	public class Classement
 	{
-		private Joueur[] j= new Joueur[4];
+		private Joueur[] j= new Joueur[16];
 
 		public Classement()
 		{
@@ -71,7 +82,9 @@ public class Ping{
 		}
 	}
 
-
+	/* Classe : Poule
+	Méthodes : Constructeur, affichageResultats
+	*/
 	public class Poule
 	{
 		private int num;
@@ -88,15 +101,114 @@ public class Ping{
 			j3=c.j[num+7];
 			j4=c.j[num+11];
 		}
-	}	
+	}
+	/* Classe : Match
+	Méthodes : Constructeur, affichage, resultat, affichageScore
+	*/
+	public class Match
+	{
+	
+	}
+	
+	/* Programme principal*/
 	public static void main (String[] args)
 	{
+		/*initialisation*/
 		Ping pong = new Ping();
-		Classement bloup= pong.new Classement();
-		bloup.tri();
-		for (int i=0;i<16;i++)
+		Classement c= pong.new Classement();
+		c.tri();
+		
+
+		/*Menu*/
+		int choix=1;
+		do
 		{
-			bloup.j[i].affich();
+			System.out.println("1) Fiches des joueurs");
+			System.out.println("2) Poules");
+			System.out.println("3) Quarts de finale");
+			System.out.println("4) Demi finales");
+			System.out.println("5) Finales");
+			System.out.println("6) Podium");
+			System.out.println("7) Sortie du programme");
+			System.out.print("Choix : ");
+			choix=sc.nextInt();
+			System.out.println("---------------");
+			switch(choix)
+			{				
+				case	1 :
+					System.out.println("Entrer le nom de joueur pour voir sa fiche ou 'tous' pour toutes les fiches");
+					String nom=sc.next();
+					if (nom.equals("tous"))
+					{
+						for(int i=0;i<16;i++)
+						{
+							c.j[i].affich();
+						}
+					}
+					else
+					{
+					
+					}
+					break;
+				case 2 :
+					int ch=1;
+					do
+					{
+						System.out.print("Choix d'une poule : ");
+						int poule=sc.nextInt();
+						if (poule>0 && poule <5)
+						{
+							System.out.println("----------------------");
+							System.out.println("1) Affichage des joueurs");
+							System.out.println("2) Affichage des matchs");
+							System.out.println("3) Affichage du classement");
+							System.out.println("4) Affichage des résultats");
+							System.out.println("5) Retour");
+							ch=sc.nextInt();
+							switch(ch)
+							{
+								case 1:
+									System.out.println("1!");
+									break;
+								
+								case 2:
+									System.out.println("2!");
+									break;
+								
+								case 3:
+									System.out.println("3!");
+									break;
+								
+								case 4:
+									System.out.println("4!");
+									break;
+							}
+						}
+						else
+						{
+							System.out.println("Entrer un numéro de poule valide : 1, 2, 3 ou 4.");
+						}
+					}
+					while (ch>0 && ch <5);
+					break;
+					
+				case 3:
+					System.out.println("3!");		
+					break;
+							
+				case 4:
+					System.out.println("4!");	
+					break;
+							
+				case 5:
+					System.out.println("5!");		
+					break;
+					
+				case 6:
+					System.out.println("6!");		
+					break;
+			}
 		}
+		while(choix>0 && choix<7);
 	}
 }
